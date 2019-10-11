25a8dd373587db842997437e3a25658ec5b7292c
silverdoggos <55032919+silverdoggos@users.noreply.github.com>
2019-10-11 15:05:37 +0300

    it's done

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -7,14 +7,62 @@
 //
 
 import UIKit
+import SwiftyJSON
+import Alamofire
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController{
+    var coinManager = CoinManager()
+    
+    @IBOutlet weak var bitcoinLable: UILabel!
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        coinManager.delegate = self
+        currencyPicker.dataSource = self
+        currencyPicker.delegate = self
+        coinManager.getCoinPrice(coinManager.currencyArray[0])
     }
-
-
 }
+//MARK: Picker View
+extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selectedCurrency: String = coinManager.currencyArray[row]
+        coinManager.getCoinPrice(selectedCurrency)
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
+    }
+}
+
+//MARK: NEWDATA
+extension ViewController: CoinManagerDelegate {
+    func didUodatePrice(price: String, currency: String){
+        DispatchQueue.main.async {
+            self.bitcoinLable.text = price
+            self.currencyLabel.text = currency
+        }
+    }
+    
+    
+    func didFailWithError(error: Error){
+        print("error")
+    }
+}
+
+
+    
+    
+
 
--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -7,10 +7,39 @@
 //
 
 import Foundation
+import Alamofire
+import SwiftyJSON
+
+protocol CoinManagerDelegate {
+    func didUodatePrice(price: String, currency: String)
+    func didFailWithError(error: Error)
+}
 
 struct CoinManager {
+    var delegate:CoinManagerDelegate?
     
-    let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
-    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    let baseURL = "https://blockchain.info/ticker"
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","INR","JPY","NZD","PLN","RUB","SEK","SGD","USD", "CHF", "CLP", "DKK", "ISK", "KRW", "THB", "TWD"]
     
+    func getCoinPrice (_ currency: String)  {
+        
+        Alamofire.request(baseURL, method: .get).responseJSON{
+            response in
+            if response.result.isSuccess{
+                let currencyJSON: JSON = JSON(response.result.value!)
+                parseJSON(currencyJSON, curr: currency)
+                print(response.result.value!)
+            } else{
+                self.delegate?.didFailWithError(error: response.result.error!)
+            }
+        }
+        
+        func parseJSON(_ data: JSON, curr: String)  {
+            if let currency = data[currency]["last"].double {
+                self.delegate?.didUodatePrice(price: "\(currency)", currency: curr)
+            } else{
+                self.delegate?.didUodatePrice(price: "Sorry", currency: "Please")
+            }
+        }
+    }
 }
